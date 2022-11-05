01fc9ff79a7f2843da0337feb9cff6d4767918a9
rearwindowsoda <mateuszowczarek@onet.eu>
2022-11-05 21:32:02 +0100

    FEATURE: added p with info that mail is

--- clipboard.js
+++ clipboard.js
@@ -1,6 +1,12 @@
 const startMail = localStorage.getItem('start-mail');
 const endMail = localStorage.getItem('end-mail');
 
+const copyInfo = (selector, text) => {
+	const par = document.getElementById(selector);
+	par.innerText = text;
+	setTimeout(() => {par.innerText = ''}, 4000)
+}
+
 const dummyInDOM = (sourceToCopy) =>  {
 	const dummyTextarea = document.createElement("textarea");
 	document.body.appendChild(dummyTextarea);
@@ -17,11 +23,13 @@ function copyToClipboard(mailVersion) {
 		case 'end-mail':
 				const copyEndMail = localStorage.getItem('end-mail');
 				dummyInDOM(copyEndMail);	
-				
+				copyInfo('p-end', 'Skopiowano do schowka.');
+
 			break;
 		case 'start-mail':
 				const copyStartMail = localStorage.getItem('start-mail');
-				dummyInDOM(copyStartMail);	
+				dummyInDOM(copyStartMail);
+				copyInfo('p-start', 'Skopiowano do schowka.');
 			break;
 		default:
 			console.error(`Something went wrong, try again later.`);
