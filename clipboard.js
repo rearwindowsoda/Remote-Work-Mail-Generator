934fc56e5776dd3b76ecf2a038bbe8da8bf283fd
rearwindowsoda <mateuszowczarek@onet.eu>
2022-11-05 03:55:13 +0100

    fixes in clipboard functions

--- clipboard.js
+++ clipboard.js
@@ -1,15 +1,29 @@
 const startMail = localStorage.getItem('start-mail');
 const endMail = localStorage.getItem('end-mail');
+
+const dummyInDOM = (sourceToCopy) =>  {
+	const dummyTextarea = document.createElement("textarea");
+	document.body.appendChild(dummyTextarea);
+	dummyTextarea.value = sourceToCopy;
+	dummyTextarea.select();
+	dummyTextarea.setSelectionRange(0,99999);
+	/*     document.execCommand("copy"); used to be that but now deprecated */
+	navigator.clipboard.writeText(dummyTextarea.value).then(document.body.removeChild(dummyTextarea)).then(alert('Saved e-mail to your clipboard.')).catch(e => {
+		console.error(e);
+		alert('Something went wrong, try again later.')
+	})
+	
+}
+
 function copyToClipboard(mailVersion) {
 	switch (mailVersion) {
 		case 'end-mail':
 				const copyEndMail = localStorage.getItem('end-mail');
-				navigator.clipboard.writeText(copyEndMail).then(console.log(copyEndMail)).then(alert("Saved e-mail to your clipboard.")).catch(e => console.log(e));
-				
+				dummyInDOM(copyEndMail);				
 			break;
 		case 'start-mail':
 				const copyStartMail = localStorage.getItem('start-mail');
-				navigator.clipboard.writeText(copyStartMail).then(console.log(copyStartMail)).then(alert("Saved e-mail to your clipboard.")).catch(e => console.log(e));
+				dummyInDOM(copyStartMail);
 			break;
 		default:
 			alert('Something went wrong. Save your e-mail and try again.')
